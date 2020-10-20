8684856970eb3368fef69f8f8e157a02180fb9db
Robin <1105080+openscript@users.noreply.github.com>
2020-10-20 23:53:30 +0200

    Add path prefix

--- .gatsby/gatsby-config.ts
+++ .gatsby/gatsby-config.ts
@@ -1,6 +1,7 @@
 import { ITSConfigFn } from 'gatsby-plugin-ts-config';
 
 const configuration: ITSConfigFn<'config'> = ({ projectRoot }) => ({
+  pathPrefix: '/openscript.ch', // TODO: Remove when it runs at the domains root
   siteMetadata: {
     title: 'openscript GmbH',
     description: 'Creative and elegant software solutions for the web and mobile devices. Performance and security optimizations of your infrastructure. Reasonable und individually.',
