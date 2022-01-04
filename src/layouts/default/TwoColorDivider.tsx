67d53b0bfe116ba35148316d4bd9d450dc5ec9fa
stampaaaron <aaron@stampa.ch>
2022-01-04 12:09:42 +0000

    cleanup twoColorDivider svg

--- src/layouts/default/TwoColorDivider.tsx
+++ src/layouts/default/TwoColorDivider.tsx
@@ -10,19 +10,21 @@ export function TwoColorDivider({ flipVertical }: TwoColorDividerProps) {
   return (
     <DividerGraphic
       css={(theme: Theme) => css`
-        background-color: ${theme.primaryColor};
         width: 100%;
         height: auto;
         z-index: 10;
         margin-bottom: -1px; // fix subpixel rendering gaps
-        transform: scaleY(${flipVertical ? -1 : 1});
+        ${flipVertical &&
+        css`
+          transform: scaleY(-1);
+        `}
 
         path:nth-child(1) {
-          fill: ${lighten(0.05, theme.primaryColor)};
+          fill: ${theme.primaryColor};
         }
 
         path:nth-child(2) {
-          fill: ${theme.backgroundColor};
+          fill: ${lighten(0.05, theme.primaryColor)};
         }
       `}
     />
