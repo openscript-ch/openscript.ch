58dbd8f9737ad335689be83931e3b317b5548a88
stampaaaron <aaron@stampa.ch>
2022-01-10 14:38:02 +0000

    display logos image

--- src/sections/ExchangeSection.tsx
+++ src/sections/ExchangeSection.tsx
@@ -2,9 +2,17 @@ import { css, Theme, useTheme } from '@emotion/react';
 import { Markup } from 'interweave';
 import { IndexPageQuery } from '../../graphql-types';
 import { DividedSection } from '../components/DividedSection';
+import { ReactComponent as Logos } from '../../content/statics/logos.svg';
 
 const sectionStyle = (theme: Theme) => css`
   display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+
+  > * {
+    flex: 1;
+    min-width: 20rem;
+  }
 
   .text-box {
     background-color: ${theme.whiteColor};
@@ -29,6 +37,7 @@ export function ExchangeSection({ exchange }: Props) {
         <h2>{exchange?.frontmatter?.title}</h2>
         <Markup content={exchange?.html} />
       </div>
+      <Logos />
     </DividedSection>
   );
 }
