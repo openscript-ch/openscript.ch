6e6c0c994ff53366f9635a75715c35c148548b3c
stampaaaron <aaron@stampa.ch>
2022-01-10 14:24:08 +0000

    display text box in ExchangeSection

--- content/data/sections/exchange/text-box.en.md
+++ content/data/sections/exchange/text-box.en.md
@@ -3,5 +3,7 @@ title: What we do
 ---
 
 In a small team we love to develop exciting software solutions of all kinds.
+
 We are specialized in modern, contemporary web applications.
+
 We are not afraid of new technologies and means to develop the perfect product for you.
--- src/sections/ExchangeSection.tsx
+++ src/sections/ExchangeSection.tsx
@@ -1,7 +1,20 @@
-import { useTheme } from '@emotion/react';
-import { PropsWithChildren } from 'react';
+import { css, Theme, useTheme } from '@emotion/react';
+import { Markup } from 'interweave';
 import { IndexPageQuery } from '../../graphql-types';
 import { DividedSection } from '../components/DividedSection';
+
+const sectionStyle = (theme: Theme) => css`
+  display: flex;
+
+  .text-box {
+    background-color: ${theme.whiteColor};
+    padding: 1rem;
+
+    h2 {
+      text-align: center;
+    }
+  }
+`;
 
 type Props = {
   exchange: IndexPageQuery['exchange'];
@@ -10,5 +23,12 @@ type Props = {
 export function ExchangeSection({ exchange }: Props) {
   const theme = useTheme();
 
-  return <DividedSection upperColor={theme.backgroundColor} lowerColor={theme.whiteColor}></DividedSection>;
+  return (
+    <DividedSection upperColor={theme.backgroundColor} lowerColor={theme.whiteColor} css={sectionStyle}>
+      <div className="text-box">
+        <h2>{exchange?.frontmatter?.title}</h2>
+        <Markup content={exchange?.html} />
+      </div>
+    </DividedSection>
+  );
 }
