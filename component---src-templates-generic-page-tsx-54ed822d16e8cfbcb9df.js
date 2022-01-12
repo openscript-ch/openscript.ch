b88c7324862d85bf8d45edee15d24781536bde63
openscript <1105080+openscript@users.noreply.github.com>
2022-01-12 15:51:26 +0000

    Deploying to gh-pages from  @ 7ac4f26ae0ac507f91afc0b70198fe6b0a6da650 🚀

---
+++ component---src-templates-generic-page-tsx-54ed822d16e8cfbcb9df.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_dogmatism=self.webpackChunkgatsby_starter_dogmatism||[]).push([[609],{4920:function(t,a,n){n.r(a),n.d(a,{default:function(){return s}});var r=n(928),u=n(945),e=n(3431);function s(t){var a,n=t.data;return(0,e.tZ)(u.H,null,(0,e.tZ)(r.Lu,{content:null===(a=n.markdownRemark)||void 0===a?void 0:a.html}))}}}]);
+//# sourceMappingURL=component---src-templates-generic-page-tsx-54ed822d16e8cfbcb9df.js.map
