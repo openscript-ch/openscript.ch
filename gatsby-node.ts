8eba9fbb30f48fc4dbc62c6b6dfc2b5d90939e48
Robin Bühler <git+r@obin.ch>
2022-04-25 11:51:01 +0000

    chore(deps): replace gatsby-ts with native typescript integration

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,10 +1,6 @@
 import { GatsbyNode } from 'gatsby';
 import { CreateGenericPages } from './src/gatsby/createPages/createGenericPages';
 
-const node: GatsbyNode = {
-  createPages: async args => {
-    await CreateGenericPages(args);
-  },
+export const createPages: GatsbyNode['createPages'] = async args => {
+  await CreateGenericPages(args);
 };
-
-export default node;
