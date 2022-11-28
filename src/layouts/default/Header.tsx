868757e4dabe1e7f79e787104ae41cf44c64c619
Robin Bühler <git+r@obin.ch>
2022-11-28 17:20:40 +0000

    chore: clean up arrow

--- src/layouts/default/Header.tsx
+++ src/layouts/default/Header.tsx
@@ -1,6 +1,5 @@
 import { css, Theme } from '@emotion/react';
 import { LocalizedLink } from 'gatsby-plugin-i18n-l10n';
-import { Arrow } from '../../components/Arrow';
 import LanguageSelector from './LanguageSelector';
 import MainNavigation from './MainNavigation';
 
@@ -21,7 +20,6 @@ type HeaderProps = {
 export function Header({ title }: HeaderProps) {
   return (
     <header css={headerStyle}>
-      <Arrow />
       <LocalizedLink to="/">{title}</LocalizedLink>
       <MainNavigation />
       <LanguageSelector />
