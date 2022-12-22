6e5fa798bbd19cb8f3c638ace93ab6ab06d59e03
stampaaaron <aaron@stampa.ch>
2022-12-22 15:38:38 +0000

    feat: implement tab logic for strengths section

--- src/sections/StrenghtsSection.tsx
+++ src/sections/StrenghtsSection.tsx
@@ -1,7 +1,33 @@
+import { Markup } from 'interweave';
+import { useState } from 'react';
+
 type Props = {
   strengths: Queries.IndexPageQuery['strengths'];
 };
 
 export function StrengthsSection({ strengths }: Props) {
-  return <article />;
+  const [selectedStrength, setSelectedStrength] = useState(strengths.nodes.find(({ frontmatter }) => frontmatter?.order === 1));
+
+  return (
+    <article>
+      {strengths.nodes.map(strength => {
+        const { order, title } = strength.frontmatter ?? {};
+        return (
+          <label htmlFor={`${order}`}>
+            {title}
+            <input
+              id={`${order}`}
+              checked={selectedStrength?.frontmatter?.order === order}
+              onChange={() => setSelectedStrength(strength)}
+              name="services"
+              type="radio"
+            />
+          </label>
+        );
+      })}
+      <div>
+        <Markup content={selectedStrength?.html} />
+      </div>
+    </article>
+  );
 }
