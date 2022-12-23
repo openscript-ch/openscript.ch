d325f6385f5b268d86950863f005458c5936ec84
stampaaaron <aaron@stampa.ch>
2022-12-23 16:10:26 +0000

    feat: display new technology logos

--- src/sections/TechnologiesSection.tsx
+++ src/sections/TechnologiesSection.tsx
@@ -1,6 +1,7 @@
 import { css, Theme } from '@emotion/react';
 import { Markup } from 'interweave';
 import { Arrow } from '../components/Arrow';
+import SpriteData from '../../content/images/sprite.json';
 import { Sprite } from '../components/Sprite';
 
 type Props = {
@@ -12,29 +13,43 @@ const sectionStyle = (theme: Theme) => css`
   gap: var(${theme.variables.gutter});
 
   .text-box {
-    flex: 2;
     background-color: ${theme.whiteColor};
     padding: var(${theme.variables.gutter});
     display: flex;
     flex-direction: column;
-
-    h2 {
-      text-align: center;
-    }
+    max-width: 30vw;
 
     svg {
       width: 4rem;
       height: auto;
-      margin-top: var(${theme.variables.gutter});
-      margin-left: auto;
+      margin-top: auto;
     }
   }
 
-  > svg {
-    flex: 3;
-    max-height: 20rem;
-    a:hover > * {
-      fill: ${theme.secondaryColor} !important;
+  .logos-grid {
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    align-items: center;
+    gap: 3rem;
+
+    a {
+      transition: transform 0.2s;
+
+      svg {
+        transition: transform 0.2s;
+        fill: white;
+        width: 5rem;
+        height: auto;
+        cursor: pointer;
+        &:hover {
+          fill: var(${theme.variables.secondaryColor});
+        }
+      }
+
+      &:hover {
+        transform: scale(1.2);
+      }
     }
   }
 
@@ -49,15 +64,52 @@ const sectionStyle = (theme: Theme) => css`
 `;
 
 export function TechnologiesSection({ exchange }: Props) {
+  const sprites: { name: keyof typeof SpriteData; url: string; size: number }[] = [
+    //
+    { name: 'logo-k0s', url: '', size: 0.5 },
+    { name: 'logo-gatsby', url: '', size: 0.8 },
+    { name: 'logo-nestjs', url: '', size: 1.1 },
+    { name: 'logo-graphql', url: '', size: 0.8 },
+    { name: 'logo-supabase', url: '', size: 0.5 },
+    //
+    { name: 'logo-docker', url: '', size: 1 },
+    { name: 'logo-ts', url: '', size: 1.1 },
+    { name: 'logo-react', url: '', size: 1.8 },
+    { name: 'logo-ruby', url: '', size: 1.1 },
+    { name: 'logo-postgres', url: '', size: 1 },
+    //
+    { name: 'logo-vscode', url: '', size: 0.5 },
+    { name: 'logo-html', url: '', size: 0.8 },
+    { name: 'logo-css', url: '', size: 1.1 },
+    { name: 'logo-rails', url: '', size: 0.8 },
+    { name: 'logo-gitea', url: '', size: 0.5 },
+  ];
+
+  const getRandomNumberBetween = (min: number, max: number) => Math.random() * (max - min) + min;
+
   return (
     <article css={sectionStyle}>
+      <div className="logos-grid">
+        {sprites.map(sprite => (
+          <a href={sprite.url}>
+            <Sprite
+              name={sprite.name}
+              css={css`
+                transform: rotate(${getRandomNumberBetween(-45, 45)}deg) scale(${sprite.size});
+                :hover {
+                  transform: rotate(0) scale(${sprite.size});
+                }
+              `}
+            />
+          </a>
+        ))}
+      </div>
       <div className="text-box">
         <h2>{exchange?.frontmatter?.title}</h2>
         <Markup content={exchange?.html} />
 
-        <Arrow rotation={240} />
+        <Arrow rotation={120} />
       </div>
-      <Sprite name="logos" />
     </article>
   );
 }
