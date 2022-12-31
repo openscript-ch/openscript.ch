c10a37a258e06bc9b0f0c606fa919d3a1b114567
Robin Bühler <git+r@obin.ch>
2022-12-31 13:12:51 +0000

    feat: configure eslint-mdx

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,50 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx', '*.json', '*.yml'],
+      extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript', 'plugin:react/jsx-runtime', 'plugin:prettier/recommended'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: ['react', '@typescript-eslint'],
+      rules: {
+        'import/prefer-default-export': 'off',
+        'import/no-relative-packages': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'react/require-default-props': 'off',
+        'react/jsx-fragments': ['error', 'element'],
+        'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      },
     },
-    ecmaVersion: 13,
-    sourceType: 'module',
-    project: './tsconfig.json'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
+    {
+      files: ['*.md', '*.mdx'],
+      extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime', 'plugin:prettier/recommended', 'plugin:mdx/recommended'],
+      plugins: ['react'],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      rules: {
+        'import/prefer-default-export': 'off',
+        'import/no-relative-packages': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'react/require-default-props': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/jsx-fragments': ['error', 'element'],
+        'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      },
+    },
   ],
-  rules: {
-    'import/prefer-default-export': 'off',
-    'import/no-relative-packages': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'react/require-default-props': 'off',
-    'react/jsx-fragments': ['error', 'element'],
-    'react/no-unknown-property': ['error', { ignore: ['css'] }]
-  },
 };
