0746a28af2e2bb10428ff14462beb50de785eafc
stampaaaron <aaron@stampa.ch>
2023-01-02 15:47:52 +0000

    feat: improve scaling of technology icons

--- src/sections/TechnologiesSection.tsx
+++ src/sections/TechnologiesSection.tsx
@@ -77,26 +77,44 @@ const sectionStyle = (theme: Theme) => css`
   }
 `;
 
+/**
+ * Calculates how near the element with the given index is to the center of the grid.
+ *
+ * @param index of the element in the grid
+ * @param cols amount in grid
+ * @param rows amount in grid
+ * @returns score from 0 - 1 how near the element is to the center of the grid.
+ */
+const getCloseToCenterScore = (index: number, cols = 5, rows = 3) => {
+  const x = index % cols;
+  const y = (index - x) / cols;
+
+  const centerCol = (cols - 1) / 2;
+  const centerRow = (rows - 1) / 2;
+
+  return (1 - (Math.abs(x - centerCol) + Math.abs(y - centerRow)) + (centerCol + centerRow - 1)) / (centerCol + centerRow);
+};
+
 export function TechnologiesSection({ exchange }: Props) {
-  const sprites: { name: keyof typeof SpriteData; url: string; size: number }[] = [
+  const sprites: { name: keyof typeof SpriteData; url: string }[] = [
     //
-    { name: 'logo-k0s', url: 'https://k0sproject.io/', size: 0.5 },
-    { name: 'logo-gatsby', url: 'https://gatsbyjs.com', size: 0.8 },
-    { name: 'logo-nestjs', url: 'https://nestjs.com', size: 1.1 },
-    { name: 'logo-graphql', url: 'https://grapqhl.org', size: 0.8 },
-    { name: 'logo-supabase', url: 'https://supabase.com', size: 0.5 },
+    { name: 'logo-k0s', url: 'https://k0sproject.io/' },
+    { name: 'logo-gatsby', url: 'https://gatsbyjs.com' },
+    { name: 'logo-nestjs', url: 'https://nestjs.com' },
+    { name: 'logo-graphql', url: 'https://grapqhl.org' },
+    { name: 'logo-supabase', url: 'https://supabase.com' },
     //
-    { name: 'logo-docker', url: 'https://docker.com', size: 1 },
-    { name: 'logo-ts', url: 'https://typescriptlang.org', size: 1.1 },
-    { name: 'logo-react', url: 'https://reactjs.org', size: 1.8 },
-    { name: 'logo-ruby', url: 'https://ruby-lang.org', size: 1.1 },
-    { name: 'logo-postgres', url: 'https://postgresql.org', size: 1 },
+    { name: 'logo-docker', url: 'https://docker.com' },
+    { name: 'logo-ts', url: 'https://typescriptlang.org' },
+    { name: 'logo-react', url: 'https://reactjs.org' },
+    { name: 'logo-ruby', url: 'https://ruby-lang.org' },
+    { name: 'logo-postgres', url: 'https://postgresql.org' },
     //
-    { name: 'logo-vscode', url: 'https://code.visualstudio.com', size: 0.5 },
-    { name: 'logo-html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML', size: 0.8 },
-    { name: 'logo-css', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS', size: 1.1 },
-    { name: 'logo-rails', url: 'https://rubyonrails.org', size: 0.8 },
-    { name: 'logo-gitea', url: 'https://gitea.io', size: 0.5 },
+    { name: 'logo-vscode', url: 'https://code.visualstudio.com' },
+    { name: 'logo-html', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+    { name: 'logo-css', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+    { name: 'logo-rails', url: 'https://rubyonrails.org' },
+    { name: 'logo-gitea', url: 'https://gitea.io' },
   ];
 
   const getRandomNumberBetween = (min: number, max: number) => Math.random() * (max - min) + min;
@@ -104,23 +122,26 @@ export function TechnologiesSection({ exchange }: Props) {
   return (
     <article css={sectionStyle}>
       <div className="logos-grid">
-        {sprites.map(sprite => (
-          <a
-            href={sprite.url}
-            css={css`
-              svg {
-                transform: rotate(${getRandomNumberBetween(-45, 45)}deg) scale(${sprite.size});
-              }
-              :hover {
+        {sprites.map((sprite, index) => {
+          const scaling = 0.7 + getCloseToCenterScore(index) ** 4 * 1.1;
+          return (
+            <a
+              href={sprite.url}
+              css={css`
                 svg {
-                  transform: rotate(0) scale(${sprite.size});
+                  transform: rotate(${getRandomNumberBetween(-45, 45)}deg) scale(${scaling});
                 }
-              }
-            `}
-          >
-            <Sprite name={sprite.name} />
-          </a>
-        ))}
+                :hover {
+                  svg {
+                    transform: rotate(0) scale(${scaling});
+                  }
+                }
+              `}
+            >
+              <Sprite name={sprite.name} />
+            </a>
+          );
+        })}
       </div>
       <div className="text-box">
         <h2>{exchange?.frontmatter?.title}</h2>
