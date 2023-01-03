1b487d7b62b97a3055e22982766ed447acacde54
Robin Bühler <git+r@obin.ch>
2023-01-03 16:48:37 +0000

    chore: clean up unused type definitions

--- src/types.d.ts
+++ src/types.d.ts
@@ -26,27 +26,3 @@ declare module '@emotion/react' {
     variables: Omit<Theme, 'variables'>;
   }
 }
-
-export type Translation = {
-  locale: string;
-  path: string;
-};
-
-export type SitePageContext = {
-  translations?: Translation[];
-  locale?: Maybe<Scalars['String']>;
-};
-
-type AlternativeLanguagePath = {
-  locale?: Maybe<Scalars['String']>;
-  path?: Maybe<Scalars['String']>;
-};
-
-type GatsbyBrowserWrapPageElementParams = Parameters<GatsbyBrowser['wrapPageElement']>;
-type GatsbySSRWrapPageElementParams = Parameters<GatsbySSR['wrapPageElement']>;
-type GatsbyBrowserWrapPageElementReturnType = ReturnType<GatsbyBrowser['wrapPageElement']>;
-type GatsbySSRWrapPageElementReturnType = ReturnType<GatsbySSR['wrapPageElement']>;
-export type WrapPageElement = (
-  args: GatsbyBrowserWrapPageElementParams[0] | GatsbySSRWrapPageElementParams[0],
-  options: PluginOptions
-) => GatsbyBrowserWrapPageElementReturnType | GatsbySSRWrapPageElementReturnType;
