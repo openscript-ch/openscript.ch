ceb4180753c85c6af2532a9a29cca3365ff96952
Robin Bühler <git+r@obin.ch>
2023-01-11 16:04:45 +0000

    chore: setup up unit testing
    Following https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  transform: {
+    '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+  },
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `<rootDir>.*/dist`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: `http://localhost`,
+  },
+  setupFiles: [`<rootDir>/loadershim.js`],
+  collectCoverage: true,
+  collectCoverageFrom: [`src/**/*.{ts,tsx}`],
+};
