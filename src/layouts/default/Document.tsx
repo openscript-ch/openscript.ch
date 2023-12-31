0ec50dbbc9b49a35002eb19c0fb3632c0509aea6
Robin Bühler <git+r@obin.ch>
2023-12-31 11:11:43 +0000

    feat: add plausible analytics

--- src/layouts/default/Document.tsx
+++ src/layouts/default/Document.tsx
@@ -1,3 +1,4 @@
+import { Script } from 'gatsby';
 import { Fragment } from 'react';
 
 type DocumentProps = {
@@ -9,6 +10,8 @@ export function Document({ metaData }: DocumentProps) {
     <Fragment>
       <title key="pageTitle">{metaData.title}</title>
       <meta key="pageDescription" name="description" content={metaData.description} />
+      <meta name="referrer" content="no-referrer-when-downgrade" />
+      <Script id="analytics" defer data-domain="openscript.ch" src="https://analytics.obin.ch/js/script.js" />
     </Fragment>
   );
 }
